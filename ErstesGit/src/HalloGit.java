72c0d21c97515d9239020eeedbff2f245866a5c2
KlajdiIsmailaj <Klajdi.Ismailaj@student.reutlingen-university.de>
2015-03-11 15:15:03 +0100

    git

--- ErstesGit/src/HalloGit.java
+++ ErstesGit/src/HalloGit.java
@@ -2,17 +2,8 @@
 public class HalloGit {
 
 	public static void main(String[] args) {
-		System.out.println("test1");
-		System.out.println("test2");
-		System.out.println("test3");
-		System.out.println("test ray ben 4 ");
-		System.out.println("test5");
-		System.out.println("test6");
-		System.out.println("test7");
-		System.out.println("test8");
-		System.out.println("test 9: radom is hazard ");
-		System.out.println("testklajdi");
-		System.out.println("jkjkdhhh");
+	efoiehfedf
+	fehfeshc
 	}
 
 }
